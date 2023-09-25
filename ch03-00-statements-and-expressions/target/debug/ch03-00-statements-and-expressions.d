3f67e202aa2bf2152a40d9f7a5af22f0fc01a51a
Fonzie-byte <twistfons@live.nl>
2023-09-26 00:44:45 +0200

    Didn't realise I already pushed this, better remove it later than never.

--- ch03-00-statements-and-expressions/target/debug/ch03-00-statements-and-expressions.d
+++
@@ -1,1 +0,0 @@
-/home/fons/Projects/rust/the_rust_book/ch03-00-statements-and-expressions/target/debug/ch03-00-statements-and-expressions: /home/fons/Projects/rust/the_rust_book/ch03-00-statements-and-expressions/src/main.rs
