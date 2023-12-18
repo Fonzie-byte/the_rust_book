a1778079ded6ab63b915ba78389797f982db190d
Fonzie-byte <twistfons@live.nl>
2023-12-18 21:33:51 +0100

    Deleted target folder

--- ch04-02-ownership-and-functions/target/debug/ch04-01-ownership-and-functions.d
+++
@@ -1,1 +0,0 @@
-/home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/target/debug/ch04-01-ownership-and-functions: /home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/src/main.rs
--- ch04-02-ownership-and-functions/target/debug/deps/ch04_01_ownership_and_functions-85a47b51d61d1d07.d
+++
@@ -1,5 +0,0 @@
-/home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/target/debug/deps/ch04_01_ownership_and_functions-85a47b51d61d1d07: src/main.rs
-
-/home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/target/debug/deps/ch04_01_ownership_and_functions-85a47b51d61d1d07.d: src/main.rs
-
-src/main.rs:
