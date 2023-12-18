bbd2424e1bcbe94444f6c44c27734f439acfc99f
Fonzie-byte <twistfons@live.nl>
2023-12-18 21:30:45 +0100

    Slowly progressing through ch 4 sometimes when I have the time in the night.

---
+++ ch04-01-ownership-and-functions/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    let s = String::from("hello"); // String comes into scope.
+
+	// `s` is still available here.
+
+    takes_ownership(s);            // String moves into function, out of scope.
+
+	// `s` is no longer available.
+
+    let x = 5;                     // Integer comes into scope.
+
+	// `x` is still available here.
+
+    makes_copy(x);                 // Integer has Copy; still in scope!
+
+	// And `x` is also available here.
+}
+
+fn takes_ownership(some_string: String) { // String moves into scope.
+	println!("{some_string}");
+} // String goes out of scope and it dropped.
+
+fn makes_copy(some_integer: i32) { // Integer copies into scope.
+	println!("{some_integer}");
+} // Integer "goes out of scope", but nothing happened.
---
+++ ch04-01-ownership-and-functions/target/debug/ch04-01-ownership-and-functions.d
@@ -0,0 +1,1 @@
+/home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/target/debug/ch04-01-ownership-and-functions: /home/fons/Projects/rust/the_rust_book/ch04-01-ownership-and-functions/src/main.rs
